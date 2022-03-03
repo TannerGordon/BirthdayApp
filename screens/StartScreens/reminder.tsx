5ce84bba2b1264f910a1d67d30a80a6743fa4deb
Tanner Gordon <tjgordon21@gmail.com>
2022-03-03 14:24:31 -0500

    updated npm

--- screens/StartScreens/reminder.tsx
+++ screens/StartScreens/reminder.tsx
@@ -7,14 +7,21 @@
 
 import { NavigationContainer } from "@react-navigation/native";
 import React, { useState, useRef } from "react";
-import { StyleSheet, Modal, View, Text, TouchableOpacity, Touchable } from "react-native";
+import {
+  StyleSheet,
+  Modal,
+  View,
+  Text,
+  TouchableOpacity,
+  Touchable,
+} from "react-native";
 import { FlatList, TextInput } from "react-native-gesture-handler";
 import { LinearGradient } from "expo-linear-gradient";
 import { Ionicons } from "@expo/vector-icons";
 import { CheckBox } from "../../components/CheckBox";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Item } from "react-native-paper/lib/typescript/components/Drawer/Drawer";
-import { Picker } from 'react-native-wheel-pick';
+import { Picker } from "react-native-wheel-pick";
 // import { connect } from "react-redux";
 // import { AppState, AppDispatch } from "../store";
 // import { RouteProp } from "@react-navigation/native";
@@ -37,7 +44,7 @@ import { Picker } from 'react-native-wheel-pick';
 export const Reminder = ({ navigation, route }: any) => {
   const { firstName, lastName, phoneNumber, day, month, year } = route.params;
   const [modalVisible, setModalVisible] = useState(false);
-  const [customDate, setCustomDate] = useState('4 days');
+  const [customDate, setCustomDate] = useState("4 days");
   const [CheckBox1, setCheckBox1] = useState(false);
   const [CheckBox2, setCheckBox2] = useState(false);
   const [CheckBox3, setCheckBox3] = useState(false);
@@ -47,27 +54,55 @@ export const Reminder = ({ navigation, route }: any) => {
 
   const [CheckedBox, setCheckedBox] = useState([false, false, false]);
 
-
-
   var initialElements = [
-    { id : "0", text : "On their birthday", custom : false},
-    { id : "1", text : "1 day before", custom : false},
-    { id : "2", text : "1 week before", custom : false},
-    { id : "3", text : "1 month before", custom : false},
-    { id : "4", text : "Custom", custom : true},
+    { id: "0", text: "On their birthday", custom: false },
+    { id: "1", text: "1 day before", custom: false },
+    { id: "2", text: "1 week before", custom: false },
+    { id: "3", text: "1 month before", custom: false },
+    { id: "4", text: "Custom", custom: true },
   ];
 
   const [exampleState, setExampleState] = useState(initialElements);
   const [idx, incr] = useState(4);
 
   const addElement = () => {
-    var pickerData=['2 days', '3 days', '4 days', '5 days', '6 days', '7 days', '8 days', '9 days', '10 days', '11 days', '12 days', '13 days', '14 days', '15 days', '16 days', '17 days', '18 days', '19 days', '20 days', '21 days', '22 days', '23 days', '24 days', '25 days', '26 days', '27 days', '28 days', '29 days', '30 days'];
+    var pickerData = [
+      "2 days",
+      "3 days",
+      "4 days",
+      "5 days",
+      "6 days",
+      "7 days",
+      "8 days",
+      "9 days",
+      "10 days",
+      "11 days",
+      "12 days",
+      "13 days",
+      "14 days",
+      "15 days",
+      "16 days",
+      "17 days",
+      "18 days",
+      "19 days",
+      "20 days",
+      "21 days",
+      "22 days",
+      "23 days",
+      "24 days",
+      "25 days",
+      "26 days",
+      "27 days",
+      "28 days",
+      "29 days",
+      "30 days",
+    ];
 
     // let str = customDate;
     var index = pickerData.indexOf(customDate) + 2;
     // console.log(index);
     var str = index.toString();
-    str = str.concat(' days before')
+    str = str.concat(" days before");
     // str = str.slice(0,2);
     // str = str.trim();
     // console.log("hey");
@@ -76,10 +111,13 @@ export const Reminder = ({ navigation, route }: any) => {
     var newNum = idx + 1;
     var newStr = newNum.toString();
     var oldStr = idx.toString();
-    
-    var newArray = [...exampleState , { id : newStr, text : "Custom", custom : true}];
+
+    var newArray = [
+      ...exampleState,
+      { id: newStr, text: "Custom", custom: true },
+    ];
     let num: any = idx;
-    newArray[num] = {id : oldStr, text: str, custom: false};
+    newArray[num] = { id: oldStr, text: str, custom: false };
     var x: number = +idx;
     x = x + 1;
     incr(newNum);
@@ -87,52 +125,49 @@ export const Reminder = ({ navigation, route }: any) => {
     setExampleState(newArray);
     console.log(newArray);
     // changeEl(newArray);
-
   };
-  
+
   const setBox = (id: number) => {
     // console.log(CheckedBox[id])
-    CheckedBox[id] = !CheckedBox[id]
-    setCheckedBox(CheckedBox)
-  }
+    CheckedBox[id] = !CheckedBox[id];
+    setCheckedBox(CheckedBox);
+  };
 
   const getBox = (id: number) => {
     // console.log("hey")
     return CheckedBox[id];
-  }
+  };
 
   const reminderItem = (text: string, custom: boolean, id: number) => {
     if (custom) {
       return (
         <TouchableOpacity
-        style={styles.checkItem}
-        onPress={() => setModalVisible(!modalVisible)}
-        activeOpacity={0.6}
-      >
-        <Text style={styles.checkboxText}>{text}</Text>
-      </TouchableOpacity>
-      )
+          style={styles.checkItem}
+          onPress={() => setModalVisible(!modalVisible)}
+          activeOpacity={0.6}
+        >
+          <Text style={styles.checkboxText}>{text}</Text>
+        </TouchableOpacity>
+      );
     }
     return (
-    <>
-      <TouchableOpacity
-        style={styles.checkItem}
-        // onPress={() => setCheckBox1(!CheckBox1)}
-        onPress = {() => setBox(id)}
-        activeOpacity={0.6}
-      >
-        {/* <CheckBox selected={CheckBox1} /> */}
-        <CheckBox selected={CheckedBox[id]}/>
-        {/* {console.log(CheckedBox[id])} */}
-        <Text style={styles.checkboxText}>{text}</Text>
-      </TouchableOpacity>
-    </>)
-  }
+      <>
+        <TouchableOpacity
+          style={styles.checkItem}
+          // onPress={() => setCheckBox1(!CheckBox1)}
+          onPress={() => setBox(id)}
+          activeOpacity={0.6}
+        >
+          {/* <CheckBox selected={CheckBox1} /> */}
+          <CheckBox selected={CheckedBox[id]} />
+          {/* {console.log(CheckedBox[id])} */}
+          <Text style={styles.checkboxText}>{text}</Text>
+        </TouchableOpacity>
+      </>
+    );
+  };
   return (
-    
     <View style={styles.container}>
-
-
       {/* <Picker
   style={{ backgroundColor: 'white', width: 300, height: 215 }}
   selectedValue='item4'
@@ -149,17 +184,52 @@ export const Reminder = ({ navigation, route }: any) => {
         //   setModalVisible(!modalVisible);
         // }}
       >
-      <View style={styles.centeredView}>
+        <View style={styles.centeredView}>
           <View style={styles.modalView}>
             <Picker
-              style={{ backgroundColor: 'white', width: 300, height: 215 }}
-              selectedValue='4 days'
-              pickerData={['2 days', '3 days', '4 days', '5 days', '6 days', '7 days', '8 days', '9 days', '10 days', '11 days', '12 days', '13 days', '14 days', '15 days', '16 days', '17 days', '18 days', '19 days', '20 days', '21 days', '22 days', '23 days', '24 days', '25 days', '26 days', '27 days', '28 days', '29 days', '30 days']}
-              onValueChange={value => { setCustomDate(value) }}
+              style={{ backgroundColor: "white", width: 300, height: 215 }}
+              selectedValue="4 days"
+              pickerData={[
+                "2 days",
+                "3 days",
+                "4 days",
+                "5 days",
+                "6 days",
+                "7 days",
+                "8 days",
+                "9 days",
+                "10 days",
+                "11 days",
+                "12 days",
+                "13 days",
+                "14 days",
+                "15 days",
+                "16 days",
+                "17 days",
+                "18 days",
+                "19 days",
+                "20 days",
+                "21 days",
+                "22 days",
+                "23 days",
+                "24 days",
+                "25 days",
+                "26 days",
+                "27 days",
+                "28 days",
+                "29 days",
+                "30 days",
+              ]}
+              onValueChange={(value) => {
+                setCustomDate(value);
+              }}
             />
             <TouchableOpacity
               style={styles.selectBtn}
-              onPress={() => {setModalVisible(!modalVisible); addElement(); }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                addElement();
+              }}
             >
               <Text style={styles.selectBtnTxt}>Select</Text>
             </TouchableOpacity>
@@ -180,18 +250,18 @@ export const Reminder = ({ navigation, route }: any) => {
         (select all that apply)
       </Text>
 
-
       {/* By passing extraData={selectedId} to FlatList we make sure FlatList itself will re-render
       //  when the state changes. Without setting this prop, FlatList would not know it needs to re-render 
       // any items because it is a PureComponent and the prop comparison will not show any changes. */}
 
-
       <SafeAreaView style={styles.safeContainer}>
         <FlatList
-          keyExtractor = {item => item.id}  
+          keyExtractor={(item) => item.id}
           data={exampleState}
           // renderItem = {item => (<Text>{item.item.text}</Text>)}
-          renderItem = {item => (reminderItem(item.item.text, item.item.custom, item.item.id))}
+          renderItem={(item) =>
+            reminderItem(item.item.text, item.item.custom, item.item.id)
+          }
           // extraData={this.state} // this is how you force flatlist to rerender
           extraData={CheckedBox}
         />
@@ -202,8 +272,7 @@ export const Reminder = ({ navigation, route }: any) => {
           <Text style={styles.question}>Add element</Text>
       </TouchableOpacity> */}
 
-
-          {/* <TouchableOpacity
+      {/* <TouchableOpacity
             style={styles.checkItem}
             onPress={() => setCheckBox1(!CheckBox1)}
             activeOpacity={0.6}
@@ -227,9 +296,6 @@ export const Reminder = ({ navigation, route }: any) => {
             <CheckBox selected={CheckBox3} />
             <Text style={styles.checkboxText}>1 week before</Text>
           </TouchableOpacity> */}
-
-
-
 
       {/* ADD CUSTOM BUTTON */}
       {/* when clicked, it brings user to another page where the user can add more */}
@@ -373,8 +439,8 @@ const styles = StyleSheet.create({
   },
 
   safeContainer: {
-    width: '100%',
-    height: '60%',
+    width: "100%",
+    height: "60%",
   },
 
   modalView: {
@@ -387,11 +453,11 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
   },
 
   centeredView: {
@@ -417,7 +483,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontFamily: "Futura",
   },
-
 });
 
 // const mapStateToProps = (state: AppState) => ({
